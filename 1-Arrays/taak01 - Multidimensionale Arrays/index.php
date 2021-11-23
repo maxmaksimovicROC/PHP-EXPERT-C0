2f0baac310a3e5a4bd06106208aaeee49a83d583
maxmaksimovic <maxmaksimovicroc@gmail.com>
2021-11-23 16:20:13 +0100

    jj

--- 1-Arrays/taak01 - Multidimensionale Arrays/index.php
+++ 1-Arrays/taak01 - Multidimensionale Arrays/index.php
@@ -1,1 +1,17 @@
+<?php
 
+
+
+$medewerkers = array (
+  array("Willem","van Oranje","koning"),
+  array("Donald","Trump","President")
+);
+
+
+echo $medewerkers[0][0]." ".$medewerkers[0][1]." is de ".$medewerkers[0][2].".<br>";
+echo $medewerkers[1][0]." ".$medewerkers[0][1]." is de ".$medewerkers[0][2].".";
+
+
+
+
+?>

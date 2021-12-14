075ccbc5b7f6abc4c8c4e68858ec935e5b103012
max maksimovic <maxmaksimovic@student.rocva.nl>
2021-12-14 10:22:37 +0100

    s

--- 3_PDO-INSERT/taak01 - Create User/config.php
+++ 3_PDO-INSERT/taak01 - Create User/config.php
@@ -8,21 +8,6 @@ $db_name = "toolsforever";
 $db = new PDO('mysql:host=localhost;dbname='. $db_name .';charset=utf8', $db_user, $db_pass);
 
 //gegevens uit een formulier ophalen met de post method.
-if(isset($_POST['create'])){
-    $firstname    = $_POST['firstname'];
-    $lastname     = $_POST['lastname'];
-    
-
--
-    $sql = "INSERT INTO user_create (firstname, lastname) VALUES(?,?)";
-     $stminsert = $db->prepare($sql);
-     $result = $stminsert->execute([$firstname, $lastname ]);    
-      if ($result) {
-        echo 'Account opgeslagen u kunt nu inloggen.';
-      }else{
-        echo 'Er ging iets fout probeer het opnieeuw.';
-      }
-  }
 
 
 ?>
--- 3_PDO-INSERT/taak01 - Create User/index.php
+++ 3_PDO-INSERT/taak01 - Create User/index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 require_once('config.php');
 
 ?>
@@ -14,23 +14,38 @@ require_once('config.php');
         <link rel="stylesheet" href="boot/css/bootstrap.min.css">
     </head>
     <body>
-        <form action="config.php" method="post">
-            <div class="container">
-          
+        <div>
+            <?php
+                    if(isset($_POST['create'])){
+                        $firstname    = $_POST['firstname'];
+                        $lastname     = $_POST['lastname'];
+                            
+
+                        -
+                        $sql = "INSERT INTO user_create (firstname, lastname) VALUES (:ph_firstname , :ph_lastname)" ;        
+                        $stmt = $db->prepare($sql); 
+                        $stmt->bindParam(":ph_firstname", $firstName ); 
+                        $stmt->bindParam(":ph_lastname", $lastName ); 
+                        $stmt->execute();  
+                    }
+            ?>
+        </div>
+        <form action="index.php" method="post">
+        
                 <div class="row">
                     <div class="col-sm-3">
                         <H1>Log In</H1>
                         <label for="firstname"><b>Firstname</b></label>
-                        <input class="form-control" type="text" placeholder="Must fill in"required>
+                        <input class="form-control" type="text" placeholder="Must fill in" required>
 
                         <label for="lastname"><b>Lastname</b></label>
-                        <input class="form-control" type="text" placeholder="Dont lie " required> 
+                        <input class="form-control" type="text" placeholder="Dont lie" required> 
                         <hr class="mb-3">
                         <input class="btn btn-primary" type="submit" name="submit" value="naam in db">   
                     </div>
                     </form>
                 </div>
-            </div>
+            
 
 
 
